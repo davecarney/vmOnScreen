c32d998640936a5462ec1beae1e541e7f8a9faf9
Dave Carney <cctvdave@gmail.com>
2018-02-21 01:03:55 -0600

    added options to allow for changing of percentage and the ability to call the function on elements other than .vm-anim

--- vmOnScreen.js
+++ vmOnScreen.js
@@ -9,24 +9,45 @@
 * 	activate with:
 * 		$('.vm-anim').vmOnScreen();
 *
-* 	watches for:
+* 	watches for elements with class:
 *		.vm-anim
 *
 * 	switches class:
 * 		.vm-anim to .vm-onscreen
 *		Target those classes to do whatever you need.
 *
+*	changing defaults:
+*		the function is set to fire when the element is at 0.75 of the screen.
+*		pass a different percentage to the percent: property to change this (example below)
+*
+*		You can overide the element to watch for by passing something else to the jQuery object
+*		but you must also pass that same element(without the . or #) to the watching property
+*
+*			$('.some-other-class').vmOnScreen({
+*				watching: 'some-other-class',
+*				percent: 0.5
+*			});
+*
+*		Use this approach if you want different elements to animate at different percentages
+*
 */
 
 (function ( $ ) {
-	$.fn.vmOnScreen = function() {
+	$.fn.vmOnScreen = function(options) {
+
+		options = $.extend({
+			percent: 0.75,
+			watching: 'vm-anim'
+		}, options);
+
+		var item = this;
 
 		function init() {
-			$('.vm-anim').each(function() {
-				var animStart = $(window).height() * 0.75;
+			$(item).each(function() {
+				var animStart = $(window).height() * options.percent;
 				var top = $(window).scrollTop();
 				if (top > ($(this).offset().top - animStart)) {
-					$(this).removeClass('vm-anim').addClass('vm-onscreen');
+					$(this).removeClass(options.watching).addClass('vm-onscreen');
 					return;
 				}
 			});
